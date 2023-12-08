d159a2d19eea28644b454da218a997fb8d28c7ec
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-08 23:29:25 +0530

    condition completed

---
+++ firstclass.java
@@ -0,0 +1,24 @@
+import java.util.*;
+//* 
+//** 
+//*** 
+//**** 
+
+class firstclass {
+    public static void main(String arg[]){
+
+        int n=4;
+        // outer loops
+        
+        for(int i=1;i<=n;i++){
+            //inner loop
+         for(int j=1;j<=i;j++){
+            System.out.print("*");
+         }
+         System.out.println();
+        }
+       
+   
+}
+
+}
---
+++ java2.java
@@ -0,0 +1,8 @@
+import java.utils.*;
+public class java2 {
+    public static void main(string arg[]){
+        system.out.print("")
+    }
+    
+    
+}
