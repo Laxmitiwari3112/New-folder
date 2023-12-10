a620e2e1de2ac0b5c253ac26ecfda4fac52eb9b8
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-10 18:48:01 +0530

    New file added

--- Problem/Sumtwovariable.java
+++ Problem/Sumtwovariable.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class Sumtwovariable {
     public static void main(String[] args) {
-        // input
+        // input by user 
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a + b;
 
+        //output
         System.out.println("Sum of a and b:" + sum);
     }
 }
