f55d8b0a1bdf33fc255caf015d4f84f2c7cc7bfd
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-11 20:27:26 +0530

    New file added

--- Practice/Functions.java
+++ Practice/Functions.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Functions {
-     public static void printMyName(String name){
+    public static void printMyName(String name) {
         System.out.println(name);
         return;
     }
-    
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String name=sc.next();
 
-        printMyName(name);//function call
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name);// function call
     }
 }
---
+++ Practice/FunctionsSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FunctionsSum {
+    public static int calculateSum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter integers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int sum=calculateSum(a,b);
+        System.out.print("Sum of given number:");
+System.out.println(sum);
+    }
+}
