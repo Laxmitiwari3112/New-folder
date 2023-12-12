cd6be6162df5d1002afa1a5b8f0d6102fc857828
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-12 22:42:05 +0530

    New file added

--- Practice/Infiniteloop.java
+++ Practice/Infiniteloop.java
@@ -3,18 +3,16 @@ import java.util.*;
 //infinite loop
 public class Infiniteloop {
     public static void main(String[] args) {
+        System.out.print("Enter num n for infinite loop");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // for (int i = 1; i <=n; i++)
+         for (int i = n; i>=n;i++)
             
-        // {
-        //     System.out.println(i);
-        // }
-        // System.out.println();
-        do {
-            System.out.println(i);
-        } while (i<n);
+         {
+             System.out.println(i);
+         }
         System.out.println();
+       
     }
 }
