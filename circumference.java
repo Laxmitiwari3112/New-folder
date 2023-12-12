86352d57e4042aaa441c20f23f615522a46e4f1d
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-12 22:44:57 +0530

    New file added

--- circumference.java
+++ circumference.java
@@ -4,14 +4,14 @@ import java.util.*;
 public class circumference {
     public static void main(String[] args) {
         System.out.print("Enter radius of circle=");
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        //int circumference=2*n*(22/7);
-        int PI=22/7;
-        int circumference=2*n*PI;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // int circumference=2*n*(22/7);
+        int  PI =22/7 ;
+        int circumference = 2 * n * PI;
 
-       // System.out.print("Circumference="+circumference);
-         System.out.println(2*n*PI);
-        
+        // System.out.print("Circumference="+circumference);
+        System.out.println(2 * n * PI);
+
     }
 }
