b84110e535fd90bb41f532765205e2f9418de976
badass1901 <shashwatsagar19@gmail.com>
2023-12-15 22:30:26 +0530

    fixed type conversion

--- CircumfernceofCircle.java
+++ CircumfernceofCircle.java
@@ -1,17 +1,19 @@
 import java.util.*;
-public class CircumfernceofCircle {
-  public static float calculateCIRCUMFERNCE(float n,float PI){
-    PI=22/7;
-   float circumference=2*n*PI;
-   return circumference;
-  }
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    float n=sc.nextFloat();
-    //float PI=22/7;
 
-    int circumference=calculateCIRCUMFERNCE(n,PI);
-    System.out.println(circumference);
+public class CircumfernceofCircle {
+  public static float calculateCIRCUMFERNCE(float n, float PI) {
+    float circumference = 2 * n * PI;
+    return circumference;
   }
-    
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    float n = sc.nextFloat();
+    float PI = (float) 22 / 7;
+
+    float circumference = calculateCIRCUMFERNCE(n, PI);
+    System.out.println(circumference);
+
+  }
+
 }
