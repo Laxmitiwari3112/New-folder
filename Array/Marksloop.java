32bf861fb06f7a282a436afb0f2331c45471312b
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-16 23:32:41 +0530

    New file added

---
+++ Array/Marksloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Marksloop {
+    public static void main(String[] args) {
+       // int[] marks=new int[3];
+        int marks[]=new int[3];//also valid
+        marks[0]=44;
+        marks[1]=56;
+    marks[2]=67;
+
+    for(int i=0;i<=3;i++){
+        System.out.println(marks[i]);
+    }
+    }
+}
