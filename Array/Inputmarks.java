38798055b8f13546baeda616e15142e616239f0f
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-16 23:59:42 +0530

    New file added

--- Array/Inputmarks.java
+++ Array/Inputmarks.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class Inputmarks {
     public static void main(String[] args) {
-        System.out.println("ENTER NUMBERS:");
+       System.out.print("INPUT SIZE OF ARRAY=");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int numbers[] = new int[size];
 
         // input
+         System.out.println("ENTER NUMBERS:");
         for (int i = 0; i < size; i++) {
             numbers[i] = sc.nextInt();
         }
