94bc86b5bbe71dde3aa728f368e4683572f4cd6a
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-17 21:39:30 +0530

    New file added

---
+++ Array/CheckSortedORnot.java
@@ -0,0 +1,41 @@
+package Array;
+import java.util.*;
+
+// create  array by user and check sorted or not
+public class CheckSortedORnot {
+    public static void main(String[] args) {
+
+        // input size of array
+        System.out.print("Enter size of array=");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        // array
+        int number[] = new int[size];
+
+        // input elements in array
+        System.out.println("Enter elements for array");
+        for (int i = 0; i < size; i++) {
+            number[i] = sc.nextInt();
+        }
+
+        // System.out.print(number[i]);
+
+        // check ascending or not
+        boolean isAscending = true;
+        for (int i = 1; i < number.length - 1; i++) {
+
+            if (number[i] > number[i + 1]) {
+                // this is fpr ascending
+                isAscending = false;
+            }
+        }
+
+        if (isAscending) {
+            System.out.println("The Array is in sorted ascending order");
+        } else {
+            System.out.println("The array is not in sorted ascending order");
+        }
+
+    }
+}
--- Array/Searchingarray.java
+++ Array/Searchingarray.java
@@ -1,46 +1,41 @@
 package Array;
+
 import java.util.*;
 
 //search max & min element in an array
 public class Searchingarray {
     public static void main(String[] args) {
 
-        //input 
+        // input
         System.out.print("Enter size of array=");
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
 
-         //array
-        int number[]=new int[size];
+        // array
+        int number[] = new int[size];
 
-        //input numbers of array
+        // input numbers of array
         System.out.println("Enter number for array");
-        for(int i=0;i<size;i++){
-            number[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            number[i] = sc.nextInt();
         }
 
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         // find max and min number in an array
         System.out.println("result are");
-        for(int i=0;i<number.length;i++){
-            // if(number[i]==max){
-            //     System.out.println("Maximum value in array is "+number[i]);
-            // }
-            // else if(number[i]==min){
-            //     System.out.println("Minimum value in array is "+number[i]);
-            // }
-            if(number[i]<min){
-                min=number[i];
+        for (int i = 0; i < number.length; i++) {
+
+            if (number[i] < min) {
+                min = number[i];
             }
-            if(number[i]>max){
-                max=number[i];
+            if (number[i] > max) {
+                max = number[i];
             }
         }
-         System.out.println("Maximum value in array is "+max);
-         System.out.println("Minimum value in array is "+min);
+        System.out.println("Maximum value in array is " + max);
+        System.out.println("Minimum value in array is " + min);
 
     }
 }
