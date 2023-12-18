5657d240164c745729d6b9422dbdf2955718bf69
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-18 23:37:16 +0530

    New file added

---
+++ Strings/Nameorsentence.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Nameorsentence {
+    public static void main(String[] args) {
+        String name="harry";//String name="--- ";
+        String sentence="java is programming language.";
+
+        //string is non premet
+    }
+}
