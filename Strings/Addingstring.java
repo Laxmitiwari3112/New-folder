28ec7fa8fd814a466e52730b86e12e2885b4ea2a
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-18 23:37:43 +0530

    New file added

---
+++ Strings/Addingstring.java
@@ -0,0 +1,50 @@
+package Strings;
+
+import java.util.*;
+
+//concatenation: addition of more than one string and print output
+//addition of string
+//comarision of string
+//seperate sub string
+public class Addingstring {
+
+    public static void main(String[] args) {
+        String firstname = "Tony";
+        String lastname = "Stark";
+        String fullname = firstname + " " + lastname;
+
+        System.out.println(fullname);
+        System.out.println(fullname.length());// no.of element=lenght of that string
+
+        // chaarAT
+        for (int i = 0; i < fullname.length(); i++) {
+            System.out.println(fullname.charAt(i));
+        }
+
+        // comparision of Strings
+        String name1 = "Tony";
+        String name2 = "Tony";
+        // 1 s1>s2 :+ve value
+        // 2 s1=s2 : 0
+        // 3 s1<s2 : -ve value
+
+        // me you (you is big string as first char y has higher),compare all characters
+
+        // name1==name2 fails in some cases so we use compareTo function
+        if (name1.compareTo(name2) == 0) {
+            System.out.println("String are equal");
+        } else {
+            System.out.println("String are not equal");
+        }
+
+        // compare
+        String sentence = "My name is Tonny";
+        // substring(beg index, end index)
+        String name = sentence.substring(11, sentence.length());
+        System.out.println(name);
+
+        String nameA = "TonyStark";
+        String nameB = nameA.substring(4);//by default end index is as last index
+        System.out.println(nameB);
+    }
+}
--- Strings/Nameorsentence.java
+++ Strings/Nameorsentence.java
@@ -1,10 +1,22 @@
 package Strings;
 
+import java.util.*;
+
 public class Nameorsentence {
     public static void main(String[] args) {
-        String name="harry";//String name="--- ";
-        String sentence="java is programming language.";
+        String name = "harry";// String name="--- ";
+        String sentence = "java is programming language.";
 
-        //string is non premet
+        // string is non primitive data type which stores data as
+        // text/sentences/characters
+        // these are CHAR,BINARY,ENUM,SET,ETC.....
+
+        // input by user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String entername = sc.next();
+        //FOR ONE WORD OUTPUT we use sc.next();
+        //FOR COMPLETE LINE we use sc.nextLine();
+        System.out.println("your name is : " + entername);
     }
 }
