1b42226211b6e054d128a754bbbfa25fbf8922de
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-19 19:49:45 +0530

    New file added

--- Strings/ToString.java
+++ Strings/ToString.java
@@ -3,5 +3,13 @@ import java.util.*;
 
 //to string method of string
 public class ToString {
-    
+    public static void main(String[] args) {
+
+        //input
+        int number=123;
+        String str=Integer.toString(number);
+
+        //output (no of integer use=?)
+        System.out.println(str.length());
+    }
 }
