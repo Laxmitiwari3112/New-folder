77e18aa13e602945974884558d73128a467918ba
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-20 21:35:29 +0530

    New file added

--- Strings/AppendCHAR.java
+++ Strings/AppendCHAR.java
@@ -1,10 +1,11 @@
 package Strings;
+
 import java.util.*;
 
 //APPEND : to add something at the end
 public class AppendCHAR {
     public static void main(String[] args) {
-        StringBuilder str1=new StringBuilder("Tony");
+        StringBuilder str1 = new StringBuilder("Tony");
         str1.append("Stark");
 
         System.out.println(str1);
