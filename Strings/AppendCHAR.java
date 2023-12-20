0834dbfd733b011129ab0194ba9f4336d756d3f4
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-20 21:35:14 +0530

    New file added

---
+++ Strings/AppendCHAR.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.util.*;
+
+//APPEND : to add something at the end
+public class AppendCHAR {
+    public static void main(String[] args) {
+        StringBuilder str1=new StringBuilder("Tony");
+        str1.append("Stark");
+
+        System.out.println(str1);
+    }
+}
