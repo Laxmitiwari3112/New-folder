50cdb25972feb9aef01c4c6e7f8f7c0db83cfa65
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2023-12-22 22:32:56 +0530

    New file added

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+//BUBBLE SORT : Pass(loop)=n-1 , traversing in each paas=n
+public class BubbleSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        // array
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // bubble sort
+        for (int i = 0; i < arr.length - 1; i++) { // array length : n-1
+            // sorting for remaining elements
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];// temperory variable
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        // output
+        printArray(arr);
+    }
+}
