de545015ef8612fcc0fd028ab0816372adb63b03
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2024-01-06 23:38:14 +0530

    New file added

---
+++ Recursion/PermutationOfString.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+//print all permutations of string
+public class PermutationOfString {
+
+    // FUNC DEFINTION
+    public static void printPerm(String str, String permutation) {
+        // BASE CASE
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            // "abc"->"ab" if currchar=c then remaining string is "ab"
+            // in substring end string are not included
+            String newstr = str.substring(0, i) + str.substring(i + 1);
+            printPerm(newstr, permutation + currChar);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        System.out.print("Given string is = ");
+        System.out.println(str);
+
+        System.out.println("All possible permutations are");
+        printPerm(str, " ");// func call
+
+    }
+}
--- Recursion/PrintKeypadCombination.java
+++ Recursion/PrintKeypadCombination.java
@@ -3,29 +3,30 @@ package Recursion;
 //TIME COMPLEXITY : O(4^n)   as here max choice=4 pqrs
 //print combinations are 0->".",1->"abc",2->"def",3->"ghi",4->"jkl",5->"mno",6->"pqrs",7->"tu",8->"vwx",9->"yz"
 public class PrintKeypadCombination {
-    public static String[] keypad={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
-    
-    //function definition
-    public static void printComb(String str, int idx, String combination){
+    public static String[] keypad = { ".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
 
-        //BASE CASE
-        if(idx==str.length()){
+    // function definition
+    public static void printComb(String str, int idx, String combination) {
+
+        // BASE CASE
+        if (idx == str.length()) {
             System.out.println(combination);
             return;
         }
-        char currChar=str.charAt(idx);
-        String mapping=keypad[currChar-'0'];
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar - '0'];
 
-        for(int i=0;i<mapping.length();i++){
-            printComb(str, idx+1, combination+mapping.charAt(i));
+        for (int i = 0; i < mapping.length(); i++) {
+            printComb(str, idx + 1, combination + mapping.charAt(i));
         }
     }
+
     public static void main(String[] args) {
-        String str="23";
+        String str = "23";
 
         System.out.println("Keypad combinations are");
-        printComb(str, 0, " ");//func call
-        
+        printComb(str, 0, " ");// func call
+
     }
-    
+
 }
