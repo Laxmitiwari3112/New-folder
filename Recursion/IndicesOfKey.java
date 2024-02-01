27facbfd94d7f135177f559d3e6077f83ef98545
Laxmitiwari3112 <laxmitiwari9651@gmail.com>
2024-02-01 21:51:44 +0530

    new file added

--- Recursion/IndicesOfKey.java
+++ Recursion/IndicesOfKey.java
@@ -1,23 +1,26 @@
 package Recursion;
+
+//print indices of key 
 public class IndicesOfKey {
-    public static void printIndicesKey(int[] arr,int key,int i){
-        //BASE CASE
-        if(i==arr.length){
+    public static void printIndicesKey(int[] arr, int key, int i) {
+        // BASE CASE
+        if (i == arr.length) {
             return;
         }
 
-        //WORK
-        if(arr[i]==key){
-            System.out.print(i+" ");
+        // WORK
+        if (arr[i] == key) {
+            System.out.print(i + " ");
         }
-        printIndicesKey(arr, key,i+1);
+        printIndicesKey(arr, key, i + 1);
     }
+
     public static void main(String[] args) {
-        int arr[]={3,2,4,5,6,2,7,2,2};
-        int key=2;
+        int arr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
+        int key = 2;
 
         System.out.print("All occurences = ");
-        printIndicesKey(arr, key,0);//0 to n indexing
+        printIndicesKey(arr, key, 0);// 0 to n indexing
         System.out.println();
     }
 }
